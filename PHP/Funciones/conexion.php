8915af0330ffe2bfc2a28f0c32066e836698d4dd
ejormej <77138125+ejormej@users.noreply.github.com>
2021-08-19 16:01:28 -0600

    Delete PHP directory

--- PHP/Funciones/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $mysqli=new mysqli("sql309.epizy.com ","epiz_29473380","XIEKhaRzZw","epiz_29473380_Pollera");
-    if(mysqli_connect_errno())
-    {
-        echo "Conexion Fallida",  mysqli_connect_errno();
-        exit();
-    }
-?> 
