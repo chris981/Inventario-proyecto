4d6124f5a25d1237d385421996bfec2b98ec7c7c
chris981 <cdams_98@hotmail.com>
2021-09-07 22:15:01 -0600

    Add files via upload

---
+++ PHP/v2.4/Funciones/conexion.php
@@ -0,0 +1,10 @@
+<?php
+    //$mysqli=new mysqli("sql309.epizy.com","epiz_29473380","XIEKhaRzZw","epiz_29473380_Pollera");
+    $mysqli=new mysqli("localhost","test","Test2020!","dbpollera");
+
+    if(mysqli_connect_errno())
+    {
+        die("Conexion Fallida". mysqli_connect_errno());
+        exit();
+    }
+?> 
---
+++ PHP/v2.4/Funciones/fmControlRBDFrituras.php
@@ -0,0 +1,67 @@
+<html>
+<html>
+
+<head>
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="../CSS/w3.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    //Definimos el tipo de accion tomada en el formulario principal
+    $Tipo_Accion = $_POST["Tipo_Accion"];
+
+    //definimos las variables que obtenemos del formulario principal
+    if (!isset($_POST["codigo"])) {
+        $codigo = "0";
+    } else {
+        $codigo = ($_POST["codigo"]);
+    }
+
+    $codigoFritura = $_POST["codigofritura"];
+    $cantidadFritura = $_POST["cantidadfritura"];
+    $codigoEmpleado = $_POST["codigoempleado"];
+    $horarioFritura = $_POST["horarioFritura"];
+    $cambioRBD = $_POST["cambioRBD"];
+    $codigoempcambio = $_POST["codigoempcambio"];
+    $codigofre = $_POST["codigofre"];
+    //Iniciamos programaciones
+    require "conexion.php";
+    try {
+        $query = "CALL SP_ControlFrituras($codigoFritura,'$cantidadFritura','$codigoempleado','$horarioFritura','$cambioRBD','$codigoempcambio','$codigofre')";
+        $resultado = $mysqli->query($query);
+        switch ($Tipo_Accion) {
+            case "I":
+    ?>
+                <h2>Cambio agregado de RBD!</h2>
+                <a href="../Formularios/ControlRBDFrituras.php">
+                    <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
+                </a>
+            <?php
+                break;
+            case "A":
+            ?>
+                <h2>Actualizacion de RBD!</h2>
+                <a href="../Formularios/ControlRBDFrituras.php">
+                    <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
+                </a>
+            <?php
+                break;
+            case "E":
+            ?>
+                <h2>RBD Eliminado!</h2>
+                <a href="../Formularios/ControlRBDFrituras.php">
+                    <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
+                </a>
+        <?php
+                break;
+        }
+    } catch (Exception $ex) {
+        ?>
+        <h2>Error: <?php echo $ex; ?></h2>
+    <?php
+    }
+    ?>
+</body>
+
+</html>
