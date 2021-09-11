565b74cacf9de54596703db500003d6b45f4fba6
ejormej <77138125+ejormej@users.noreply.github.com>
2021-09-10 21:54:47 -0600

    Add files via upload

--- Query MySQL.sql
+++ Query MySQL.sql
@@ -1,17 +1,23 @@
 create database dbPollera;
-use dbpollera;
+ 
 
+use dbpollera;
+ 
+
+-- Creacion De Tablas
+
+--  SQLINES DEMO *** rituras
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 create table Tabla_Control_Frituras
 (
-	tot_fri int auto_increment not null primary key,
+	cod_fritura int auto_increment not null primary key,
 	cant_fri int not null,
-	info_fritura varchar(60) not null,
-	cant_comida int not null,
-	nom_coc varchar(60) not null,
-	hor_fri datetime(3) not null,
-	cam_rbd datetime(3) not null,
-	nom_cam_rbd varchar(60) not null
-);
+	cod_emp int,
+	hor_fri datetime(3),
+	cam_rbd datetime(3),
+	cod_emp_cam int,
+	cod_fre int
+	);
 
 -- Tabla Menu Productos
 -- SQLINES LICENSE FOR EVALUATION USE ONLY
@@ -23,6 +29,8 @@ create table Tabla_Menu
 	tip_prod varchar(9)
 );
 
+-- Tabla Suministros
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 create table Tabla_Suministros
 (
 	cod_sum int auto_increment not null primary key,
@@ -32,45 +40,43 @@ create table Tabla_Suministros
 	elab_sum datetime(3) not null,
 	venc_sum datetime(3) not null,
 	rec_sum datetime(3) not null,
-	emp_rec varchar(30) not null
+	cod_emp int
 );
 
-create table Tabla_Suministros_Frios
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+create table Tipo_Suministros
 (
 	cod_sum int auto_increment not null primary key,
-	nom_sum varchar(60) not null,
-	cant_inv int not null,
-	elab_sum datetime(3) not null,
-	venc_sum datetime(3) not null,
-	rec_sum datetime(3) not null,
-	emp_rec varchar(30) not null
+	nom_sum varchar(60) not null
 );
 
-create table Tabla_Suministros_Secos
-(
-	cod_sum int auto_increment not null primary key,
-	nom_sum varchar(60) not null,
-	cant_inv int not null,
-	elab_sum datetime(3) not null,
-	venc_sum datetime(3) not null,
-	rec_sum datetime(3) not null,
-	emp_rec varchar(30) not null
-);
-
+-- Tabla Ventas
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 create table Tabla_Ventas
 (
 	factura int auto_increment not null primary key,
 	fech_venta datetime(3) not null,
-	nom_emp varchar(30) not null,
-	coor_resp varchar(30) not null,
+	cod_emp int not null,
 	cant_vend int not null,
-	det_ven varchar(60) not null,
 	tot_venta decimal(10,2) not null
 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+create table Detalle_ventas
+(
+	factura int auto_increment not null primary key,
+	cod_sum int,
+	cod_menu int
+);
+
+
+-- Tabla Empleados
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 create table Tabla_Empleados
 (
 	cod_emp int auto_increment not null primary key,
+	usu_emp varchar(30) not null,
+	cont_emp varchar(30) not null,
 	nom_emp varchar(30) not null,
 	ape_emp varchar(30) not null,
 	res_emp varchar(30),
@@ -79,86 +85,84 @@ create table Tabla_Empleados
 	cant_din_ventas decimal(10,2)
 );
 
-create table Tabla_Usuarios
-(
-	cod_emp int auto_increment not null primary key,
-	nom_emp varchar(30) not null,
-	ape_emp varchar(30) not null,
-	tip_usu varchar(15) not null,
-	usu_emp varchar(60) not null,
-	con_usu varchar(60) not null
-);
 
-create table Tabla_Pedidos
+-- Tabla Pedidos
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+create table Tabla_Solicitud_Pedidos
 (
 	num_ped int auto_increment not null primary key,
 	nom_sum varchar(30) not null,
 	fecha datetime(3) not null,
 	tip_sum varchar(9) not null,
 	cant_soli int not null, 
-	enc_pedido varchar(30) not null
+	cod_emp int
 );
 
+--  SQLINES DEMO ***  Pedidos
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 create table Tabla_Recepcion_Pedidos
 (
 	num_ped int auto_increment not null primary key,
-	nom_sum varchar(30) not null,
 	fecha datetime(3) not null,
-	tip_sum varchar(9) not null,
 	cant_reci int not null, 
-	enc_recepcion varchar(30) not null,
+	cod_emp int not null,
 	tot_gasto decimal(10,2) not null
 );
 
+--  SQLINES DEMO *** Compras
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 create table Tabla_Registro_Compras
 (
 	num_com int auto_increment not null primary key,
 	nom_art varchar(30) not null,
 	fecha datetime(3) not null,
 	tot_gast decimal(10,2) not null,
-	enc_compra varchar(30) not null
+	cod_emp int
 );
 
---  SQLINES DEMO *** astos
+-- Tabla Freidoras
 -- SQLINES LICENSE FOR EVALUATION USE ONLY
-create table Tabla_Control_Gastos
+create table Tabla_Freidoras
 (
-	num_gast int auto_increment not null primary key,
-	tot_gast decimal(10,2) not null
+	cod_fre int auto_increment not null primary key,
+	info_fre varchar(30) not null
 );
 
--- set dateformat dmy;
+-- SQLINES DEMO *** mato Fechas (Dia,Mes,Año)
 
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Control Frituras
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
 create procedure SP_ControlFrituras
 (
+p_cod_fritura int,
 p_cant_fri int,
-p_info_fritura varchar(60),
-p_cant_comida int,
-p_nom_coc varchar(60),
+p_cod_emp int,
 p_hor_fri datetime(3),
 p_cam_rbd datetime(3),
-p_nom_cam_rbd varchar(60),
+p_cod_emp_cam int,
+p_cod_fre int,
 p_modo char(1)
 )
 begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Control_Frituras values (p_cant_fri,p_info_fritura,p_cant_comida,p_nom_coc,p_hor_fri,p_cam_rbd,p_nom_cam_rbd);
+insert into Tabla_Control_Frituras values (p_cant_fri,p_cod_emp,p_hor_fri,p_cam_rbd,p_cod_emp_cam,p_cod_fre);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Control_Frituras set cant_fri=p_cant_fri,info_fritura=p_info_fritura,cant_comida=p_cant_comida,nom_coc=p_nom_coc,hor_fri=p_hor_fri,cam_rbd=p_cam_rbd,nom_cam_rbd=p_nom_cam_rbd
-where cant_fri=p_cant_fri;
+update Tabla_Control_Frituras set cant_fri=p_cant_fri,cod_emp=p_cod_emp,hor_fri=p_hor_fri,cam_rbd=p_cam_rbd,cod_emp_cam=p_cod_emp_cam,cod_fre=p_cod_fre
+where cod_fritura=p_cod_fritura;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_Control_Frituras
-where cant_fri=p_cant_fri;
+where cod_fritura=p_cod_fritura;
 end if;
 
 end;
@@ -166,13 +170,18 @@ end;
 
 delimiter ;
 
+
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Menu Productos
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
 create procedure SP_Menu
 (
+p_cod_menu int,
 p_nom_menu varchar(60),
 p_pre_menu int,
-p_tip_prod varchar(9),
+p_tip_prod varchar(9), 
 p_modo char(1)
 )
 begin
@@ -185,13 +194,13 @@ end if;
 if p_modo='A'
 then
 update Tabla_menu set nom_menu=p_nom_menu,pre_menu=p_pre_menu,tip_prod=p_tip_prod
-where nom_menu=p_nom_menu;
+where cod_menu=p_cod_menu;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_menu
-where nom_menu=p_nom_menu;
+where cod_menu=p_cod_menu;
 end if;
 
 end;
@@ -199,36 +208,41 @@ end;
 
 delimiter ;
 
+
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Suministros
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
 create procedure SP_Suministros
 (
+p_cod_sum int,
 p_nom_sum varchar(60),
 p_tip_sum varchar(9),
 p_cant_inv int,
 p_elab_sum datetime(3),
 p_venc_sum datetime(3),
 p_rec_sum datetime(3),
-p_emp_rec varchar(30),
+p_cod_emp int,
 p_modo char(1)
 )
 begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Suministros values (p_nom_sum,p_tip_sum,p_cant_inv,p_elab_sum,p_venc_sum,p_rec_sum,p_emp_rec);
+insert into Tabla_Suministros values (p_nom_sum,p_tip_sum,p_cant_inv,p_elab_sum,p_venc_sum,p_rec_sum,p_cod_emp);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Suministros set nom_sum=p_nom_sum,tip_sum=p_tip_sum,cant_inv=p_cant_inv,elab_sum=p_elab_sum,venc_sum=p_venc_sum,rec_sum=p_rec_sum,emp_rec=p_emp_rec
-where nom_sum=p_nom_sum;
+update Tabla_Suministros set nom_sum=p_nom_sum,tip_sum=p_tip_sum,cant_inv=p_cant_inv,elab_sum=p_elab_sum,venc_sum=p_venc_sum,rec_sum=p_rec_sum,cod_emp=p_cod_emp
+where cod_sum=p_cod_sum;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_Suministros
-where nom_sum=p_nom_sum;
+where cod_sum=p_cod_sum;
 end if;
 
 end;
@@ -236,87 +250,19 @@ end;
 
 delimiter ;
 
-delimiter //
 
-create procedure SP_Suministros_Frios
-(
-p_nom_sum varchar(60),
-p_cant_inv int,
-p_elab_sum datetime(3),
-p_venc_sum datetime(3),
-p_rec_sum datetime(3),
-p_emp_rec varchar(30),
-p_modo char(1)
-)
-begin
--- Inserccion
-if p_modo='I'
-then
-insert into Tabla_Suministros_Frios values (p_nom_sum,p_cant_inv,p_elab_sum,p_venc_sum,p_rec_sum,p_emp_rec);
-end if;
--- Actualizacion
-if p_modo='A'
-then
-update Tabla_Suministros_Frios set nom_sum=p_nom_sum,cant_inv=p_cant_inv,elab_sum=p_elab_sum,venc_sum=p_venc_sum,rec_sum=p_rec_sum,emp_rec=p_emp_rec
-where nom_sum=p_nom_sum;
-end if;
--- Eliminacion
-if p_modo='E'
-then 
-delete from Tabla_Suministros_Frios
-where nom_sum=p_nom_sum;
-end if;
 
-end;
-//
 
-delimiter ;
-
-delimiter //
-
-create procedure SP_Suministros_Secos
-(
-p_nom_sum varchar(60),
-p_cant_inv int,
-p_elab_sum datetime(3),
-p_venc_sum datetime(3),
-p_rec_sum datetime(3),
-p_emp_rec varchar(30),
-p_modo char(1)
-)
-begin
--- Inserccion
-if p_modo='I'
-then
-insert into Tabla_Suministros_Secos values (p_nom_sum,p_cant_inv,p_elab_sum,p_venc_sum,p_rec_sum,p_emp_rec);
-end if;
--- Actualizacion
-if p_modo='A'
-then
-update Tabla_Suministros_Secos set nom_sum=p_nom_sum,cant_inv=p_cant_inv,elab_sum=p_elab_sum,venc_sum=p_venc_sum,rec_sum=p_rec_sum,emp_rec=p_emp_rec
-where nom_sum=p_nom_sum;
-end if;
--- Eliminacion
-if p_modo='E'
-then 
-delete from Tabla_Suministros_Secos
-where nom_sum=p_nom_sum;
-end if;
-
-end;
-//
-
-delimiter ;
-
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Ventas
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
 create procedure SP_Ventas
 (
+p_factura int,
 p_fech_venta datetime(3),
-p_nom_emp varchar(30),
-p_coor_resp varchar(30),
+p_cod_emp int,
 p_cant_vend int,
-p_det_ven varchar(60),
 p_tot_venta decimal(10,2),
 p_modo char(1)
 )
@@ -324,19 +270,19 @@ begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Ventas values (p_fech_venta,p_nom_emp,p_coor_resp,p_cant_vend,p_det_ven,p_tot_venta);
+insert into Tabla_Ventas values (p_fech_venta,p_cod_emp,p_cant_vend,p_tot_venta);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Ventas set fech_venta=p_fech_venta,nom_emp=p_nom_emp,coor_resp=p_coor_resp,cant_vend=p_cant_vend,det_ven=p_det_ven,tot_venta=p_tot_venta
-where fech_venta=p_fech_venta;
+update Tabla_Ventas set fech_venta=p_fech_venta,cod_emp=p_cod_emp,cant_vend=p_cant_vend,tot_venta=p_tot_venta
+where factura=p_factura;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_Ventas
-where fech_venta=p_fech_venta;
+where factura=p_factura;
 end if;
 
 end;
@@ -344,35 +290,81 @@ end;
 
 delimiter ;
 
+
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Detalle Ventas
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
-create procedure SP_Empleados
+create procedure SP_Detalle_Ventas
 (
-p_nom_emp varchar(30),
-p_ape_emp varchar(30),
-p_res_emp varchar(30),
-p_hor_emp varchar(9),
-p_cant_ventas int,
-p_cant_din_ventas decimal(10,2),
+p_factura int,
+p_cod_sum int,
+p_cod_menu int,
 p_modo char(1)
 )
 begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Empleados values (p_nom_emp,p_ape_emp,p_res_emp,p_hor_emp,p_cant_ventas,p_cant_din_ventas);
+insert into Detalle_ventas values (p_cod_sum,p_cod_menu);
+end if;
+-- Actualizacion
+if p_modo='A'
+then
+update Detalle_ventas set cod_sum=p_cod_sum,cod_menu=p_cod_menu
+where factura=p_factura;
+end if;
+-- Eliminacion
+if p_modo='E'
+then 
+delete from Detalle_ventas
+where factura=p_factura;
+end if;
+
+end;
+//
+
+delimiter ;
+
+
+
+
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Empleados
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+delimiter //
+
+create procedure SP_Empleados
+(
+	cod_emp int,
+	usu_emp varchar(30) ,
+	cont_emp varchar(30) ,
+	nom_emp varchar(30) ,
+	ape_emp varchar(30) ,
+	res_emp varchar(30),
+	hor_emp varchar(9),
+	cant_ventas int,
+	cant_din_ventas decimal(10,2),
+    p_modo char(1)
+)
+begin
+-- Inserccion
+if p_modo='I'
+then
+insert into Tabla_Empleados(usu_emp,cont_emp,nom_emp,ape_emp,res_emp,hor_emp,cant_ventas,cant_din_ventas) values (usu_emp,cont_emp,nom_emp,ape_emp,res_emp,hor_emp,cant_ventas,cant_din_ventas);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
 update Tabla_Empleados set nom_emp=p_nom_emp,ape_emp=p_ape_emp,res_emp=p_res_emp,hor_emp=p_hor_emp,cant_ventas=p_cant_ventas,cant_din_ventas=p_cant_din_ventas
-where nom_emp=p_nom_emp;
+where cod_emp=p_cod_emp;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_Empleados
-where nom_emp=p_nom_emp;
+where cod_emp=p_cod_emp;
 end if;
 
 end;
@@ -380,34 +372,40 @@ end;
 
 delimiter ;
 
+
+
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Solicitud Pedidos
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
-create procedure SP_Usuarios
+create procedure SP_Solicitud_Pedidos
 (
-p_nom_emp varchar(30),
-p_ape_emp varchar(30),
-p_tip_usu varchar(15),
-p_usu_emp varchar(60),
-p_con_usu varchar(60),
+p_num_ped int,
+p_nom_sum varchar(30),
+p_fecha datetime(3),
+p_tip_sum varchar(9),
+p_cant_soli int,
+p_cod_emp int,
 p_modo char(1)
 )
 begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Usuarios values (p_nom_emp,p_ape_emp,p_tip_usu,p_usu_emp,p_con_usu);
+insert into Tabla_Solicitud_Pedidos values (p_nom_sum,p_fecha,p_tip_sum,p_cant_soli,p_cod_emp);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Usuarios set nom_emp=p_nom_emp,ape_emp=p_ape_emp,tip_usu=p_tip_usu,usu_emp=p_usu_emp,con_usu=p_con_usu
-where nom_emp=p_nom_emp;
+update Tabla_Solicitud_Pedidos set nom_sum=p_nom_sum,fecha=p_fecha,tip_sum=p_tip_sum,cant_soli=p_cant_soli,cod_emp=p_cod_emp
+where num_ped=p_num_ped;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
-delete from Tabla_Usuarios
-where nom_emp=p_nom_emp;
+delete from Tabla_Solicitud_Pedidos
+where num_ped=p_num_ped;
 end if;
 
 end;
@@ -415,50 +413,18 @@ end;
 
 delimiter ;
 
+
+
+-- SQLINES DEMO *** iento Almacenado Para Mantenimiento Tabla Recepcion Pedidos
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
-create procedure SP_Pedidos
+create procedure SP_Recepcion_Compras
 (
-p_nom_sum varchar(30),
+p_num_ped int,
 p_fecha datetime(3),
-p_tip_sum varchar(9),
-p_cant_soli int, 
-p_enc_pedido varchar(30),
-p_modo char(1)
-)
-begin
--- Inserccion
-if p_modo='I'
-then
-insert into Tabla_Pedidos values (p_nom_sum,p_fecha,p_tip_sum,p_cant_soli,p_enc_pedido);
-end if;
--- Actualizacion
-if p_modo='A'
-then
-update Tabla_Pedidos set nom_sum=p_nom_sum,fecha=p_fecha,tip_sum=p_tip_sum,cant_soli=p_cant_soli,enc_pedido=p_enc_pedido
-where nom_sum=p_nom_sum;
-end if;
--- Eliminacion
-if p_modo='E'
-then 
-delete from Tabla_Pedidos
-where nom_sum=p_nom_sum;
-end if;
-
-end;
-//
-
-delimiter ;
-
-delimiter //
-
-create procedure SP_Recepcion_Pedidos
-(
-p_nom_sum varchar(30),
-p_fecha datetime(3),
-p_tip_sum varchar(9),
 p_cant_reci int, 
-p_enc_recepcion varchar(30),
+p_cod_emp int,
 p_tot_gasto decimal(10,2),
 p_modo char(1)
 )
@@ -466,19 +432,19 @@ begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Recepcion_Pedidos values (p_nom_sum,p_fecha,p_tip_sum,p_cant_reci,p_enc_recepcion,p_tot_gasto);
+insert into Tabla_Recepcion_Pedidos values (p_fecha,p_cant_reci,p_cod_emp,p_tot_gasto);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Recepcion_Pedidos set nom_sum=p_nom_sum,fecha=p_fecha,tip_sum=p_tip_sum,cant_reci=p_cant_reci,enc_recepcion=p_enc_recepcion,tot_gasto=p_tot_gasto
-where nom_sum=p_nom_sum;
+update Tabla_Recepcion_Pedidos set fecha=p_fecha,cant_reci=p_cant_reci,cod_emp=p_cod_emp,tot_gasto=p_tot_gasto
+where num_ped=p_num_ped;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_Recepcion_Pedidos
-where nom_sum=p_nom_sum;
+where num_ped=p_num_ped;
 end if;
 
 end;
@@ -486,33 +452,38 @@ end;
 
 delimiter ;
 
+
+
+--  SQLINES DEMO *** cenado Para Tabla Registro De Compras
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
 create procedure SP_Registro_Compras
 (
+p_num_com int,
 p_nom_art varchar(30),
 p_fecha datetime(3),
 p_tot_gast decimal(10,2),
-p_enc_compra varchar(30),
+p_cod_emp int,
 p_modo char(1)
 )
 begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Registro_Compras values (p_nom_art,p_fecha,p_tot_gast,p_enc_compra);
+insert into Tabla_Registro_Compras values (p_nom_art,p_fecha,p_tot_gast,p_cod_emp);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Registro_Compras set nom_art=p_nom_art,fecha=p_fecha,tot_gast=p_tot_gast,enc_compra=p_enc_compra
-where nom_art=p_nom_art;
+update Tabla_Registro_Compras set nom_art=p_nom_art,fecha=p_fecha,tot_gast=p_tot_gast,cod_emp=p_cod_emp
+where num_com=p_num_com;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
 delete from Tabla_Registro_Compras
-where nom_art=p_nom_art;
+where num_com=p_num_com;
 end if;
 
 end;
@@ -520,33 +491,75 @@ end;
 
 delimiter ;
 
+
+
+--  SQLINES DEMO *** cenado Para Tabla freidoras
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 delimiter //
 
-create procedure SP_Control_Gastos
+create procedure SP_freidoras
 (
-p_tot_gast decimal(10,2),
+p_cod_fre int,
+p_info_fre varchar(30),
 p_modo char(1)
 )
 begin
 -- Inserccion
 if p_modo='I'
 then
-insert into Tabla_Control_Gastos values (p_tot_gast);
+insert into Tabla_Freidoras values (p_info_fre);
 end if;
 -- Actualizacion
 if p_modo='A'
 then
-update Tabla_Control_Gastos set tot_gast=p_tot_gast
-where tot_gast=p_tot_gast;
+update Tabla_Freidoras set info_fre=p_info_fre
+where cod_fre=p_cod_fre;
 end if;
 -- Eliminacion
 if p_modo='E'
 then 
-delete from Tabla_Control_Gastos
-where tot_gast=p_tot_gast;
+delete from Tabla_Freidoras
+where cod_fre=p_cod_fre;
 end if;
 
 end;
 //
 
 delimiter ;
+
+
+
+--  SQLINES DEMO *** cenado Para Tabla Tipo Suministros
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+delimiter //
+
+create procedure SP_Tip_Suministros
+(
+p_cod_sum int,
+p_nom_sum varchar(60),
+p_modo char(1)
+)
+begin
+-- Inserccion
+if p_modo='I'
+then
+insert into Tipo_Suministros values (p_nom_sum);
+end if;
+-- Actualizacion
+if p_modo='A'
+then
+update Tipo_Suministros set nom_sum=p_nom_sum
+where cod_sum=p_cod_sum;
+end if;
+-- Eliminacion
+if p_modo='E'
+then 
+delete from Tipo_Suministros
+where cod_sum=p_cod_sum;
+end if;
+
+end;
+//
+
+delimiter ;
+
