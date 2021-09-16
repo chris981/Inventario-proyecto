c3f82e7507d698e6c405cf979c15e8e78c03c14f
ErikJEV <54609692+ErikJEV@users.noreply.github.com>
2021-09-16 16:31:59 -0600

    Menu modificado para otras pantallas y mSuministro

---
+++ PHP/FINAL/Formularios/mSuministros.php
@@ -0,0 +1,171 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['usuario']))
+	{
+		header("location: ../index.php");	
+    }else
+    {
+    require "../Funciones/conexion.php";
+    try {
+        $cod_sum="";
+        $nom_sum="";
+        $tip_sum="";
+        $cant_inv="";
+        $elab_sum="";
+        $venc_sum="";
+        $rec_sum="";
+        $cod_emp=$_SESSION['ID'];
+            
+        if(isset($_GET['codigo_sum'])){
+            $query2 = "SELECT * from tabla_suministros where cod_sum='" . $_GET['codigo_sum'] . "'";
+            $resultado2 = $mysqli->query($query2);
+            while ($row2=$resultado2->fetch_assoc())
+            {
+                $cod_sum=$row2['cod_sum'];
+                $nom_sum=$row2['nom_sum'];
+                $tip_sum=$row2['tip_sum'];
+                $cant_inv=$row2['cant_inv'];
+                $elab_sum=$row2['elab_sum'];
+                $venc_sum=$row2['venc_sum'];
+                $rec_sum=$row2['rec_sum'];
+                $cod_emp=$_SESSION['ID'];
+            }
+        }
+        $query = "SELECT * from tabla_suministros";
+        $resultado = $mysqli->query($query);
+    }catch(Exception $ex){}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="../CSS/w3.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+        // Copiar siempre
+        include "./menu.php";
+    ?>
+    <div class="w3-card w3-margin w3-center">
+        <b>
+            <h2>Solicitud de Pedidos</h2>
+        </b>
+    </div>
+    <div class="w3-container">
+        <form id action="../Funciones/fmSuministros.php" method="POST">
+            <div class="w3-container w3-cell w3-border">
+                <table class=" w3-table">
+                    <tr>
+                        <td><b>Codigo Suministro:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="cod_sum" value="<?php echo $cod_sum ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Nombre Suministro:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="nom_sum" value="<?php echo $nom_sum ?>" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Tipo Suministro:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="tip_sum" value="<?php echo $tip_sum ?>" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Cantidad Inventario:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="cant_inv" value="<?php echo $cant_inv ?>" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Elaboracion Suministro:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="elab_sum" value="<?php echo $elab_sum ?>" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Vencimiento Suministro:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="venc_sum" value="<?php echo $venc_sum ?>" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Recepcion Suministro:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="rec_sum" value="<?php echo $rec_sum ?>" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Codigo Empleado:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="cod_emp" value="<?php echo $_SESSION['ID']; ?>" readonly>
+                        </td>
+                    </tr>
+                </table>
+                <div>
+                    <a class="w3-button w3-round w3-orange w3-section"  href='../Formularios/mSuministros.php'>Nuevo</a>
+                    <button class="w3-button w3-round w3-orange w3-section" type="submit" name="Tipo_Accion" value="I">Agregar</button>
+                    <button class="w3-button w3-round w3-orange w3-section" type="submit" name="Tipo_Accion" value="A">Modificar</button>
+                    <button class="w3-button w3-round w3-orange w3-section" type="submit" name="Tipo_Accion" value="E">Eliminar</button>
+                    <button class="w3-button w3-round w3-orange w3-section" type="reset" value="reset">Cancelar</button>
+                </div>
+            </div>
+            <div class="w3-container w3-cell ">
+                <table class="w3-table-all">
+                    <?php
+
+                            //Codigo PHP para Tablas
+                                echo 
+                                    "<tr>
+                                        <th>Codigo Suminisitros</th>
+                                        <th>Nombre Suminitro</th>
+                                        <th>Tipo Suministro</th>
+                                        <th>Cantidad Inventario</th>
+                                        <th>Elaboracion Suministro</th>
+                                        <th>Vencimiento Suministro</th>
+                                        <th>Recepcion Suministro</th>
+                                        <th>Codigo Empleado</th>
+                                        <th></th>
+                                    <tr/>";
+                                   while($row=$resultado->fetch_assoc()){?>
+                    <tr>
+                        <td><?php echo $row['cod_sum'];?> </td>
+                        <td><?php echo $row['nom_sum']; ?> </td>
+                        <td><?php echo $row['tip_sum']; ?> </td>
+                        <td><?php echo $row['cant_inv']; ?> </td>
+                        <td><?php echo $row['elab_sum']; ?> </td>
+                        <td><?php echo $row['venc_sum']; ?> </td>
+                        <td><?php echo $row['rec_sum']; ?> </td>
+                        <td><?php echo $row['cod_emp']; ?> </td>
+                        <td>
+                            <center><a href="./mSuministros.php?codigo_sum=<?php echo $row['cod_sum'];?>">Seleccionar</a>
+                            </center>  
+                        </td>
+                    </tr>
+                    <?php }
+                    ?>
+                </table>
+            </div>
+
+        </form>
+    </div>
+</body>
+<script>
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</html>
+<?php }
+?>
--- PHP/FINAL/Formularios/menu.php
+++ PHP/FINAL/Formularios/menu.php
@@ -9,14 +9,14 @@
         <a class="w3-bar-item w3-button w3-hide-small" href="../Formularios/mSolicitudPedidos.php">Solicitud de Pedidos </a>
         <a class="w3_bar-item w3-button w3-hide-small" href="mControlRBDFrituras.php">Control RBD Frituras</a>
         <a class="w3_bar-item w3-button w3-hide-small" href="../Formularios/mVentas.php">Ventas</a>
-        <a class="w3_bar-item w3-button w3-hide-small" href="">Freidora</a>
-        <a class="w3-bar-item w3-button w3-hide-small" href="">Suministros</a>
+        <a class="w3_bar-item w3-button w3-hide-small" href="../Formularios/mFreidora.php">Freidora</a>
+        <a class="w3-bar-item w3-button w3-hide-small" href="../Formularios/mSuministros.php">Suministros</a>
         <a class="w3_bar-item w3-button w3-hide-small" href="../Funciones/logout.php">Salir</a>
         <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" href="javascript:void(0)" onclick="myFunction()">&#9776;</a>
         <div id="demo" class="w3-bar-block w3-orange w3-hide w3-hide-large w3-hide-medium">
             <a class="w3-bar-item w3-button" href="../Formularios/mSolicitudPedidos.php">Control Mensual </a>
             <a class="w3-bar-item w3-button" href="../Formularios/mControlRBDFrituras.php">Control RBD Frituras</a>
-            <a class="w3-bar-item w3-button" href="#">Ventas</a>
+            <a class="w3-bar-item w3-button" href="../Formularios/mVentas.php">Ventas</a>
             <a class="w3-bar-item w3-button" href="../Formularios/Freidoras.php">Freidora</a>
             <a class="w3-bar-item w3-button" href="../Formularios/Suministros.php">Suministros</a>
             <a class="w3_bar-item w3-button w3-hide-small" href="../Funciones/logout.php">Salir</a>
