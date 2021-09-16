1a7923cb0ee3af1a5f34c42836df664e2a49f3f8
Norman-Villalvir5 <55332699+Norman-Villalvir5@users.noreply.github.com>
2021-09-16 12:10:17 -0600

    pantalla norman

---
+++ PHP/FINAL/Formularios/mVentas.php
@@ -0,0 +1,192 @@
+
+<?php
+	session_start();
+	if (!isset($_SESSION['usuario'])) {
+
+  header("location: ../index.php");
+} else 
+{
+  require "../Funciones/conexion.php" ;
+ 
+  try {
+    
+   $cod_emp=$_SESSION ['id']; 
+   $factura="";
+    $fecha="";
+    $cantidad="";
+    $total="";
+
+
+
+
+    if(isset($_GET['id'])){
+      $query2 = "SELECT * from tabla_ventas where factura='" . $_GET['id']."'";
+      $resultado2 = $mysqli->query($query2);
+      while ($row2=$resultado2->fetch_assoc())
+      {
+        $cod_emp=$row2['cod_emp'];  
+        $factura= $row2['factura'];
+        $fecha= $row2['fech_venta'];
+        $cantidad=$row2['cant_vend'];
+        $total=$row2['tot_venta'];
+    }
+
+    }
+    $query = "SELECT * from tabla_ventas";
+    $resultado = $mysqli->query($query);
+  } catch (Exception $ex) {
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+    
+
+<head>
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="../CSS/w3.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="w3-bar w3-orange">
+        <b><a class="w3-bar-item w3-button" href="../Formularios/Main.php">Inicio</a></b>
+        <div class="w3-dropdown-hover">
+            <button class="w3-button">Guardar</button>
+            
+        </div>
+        <a class="w3-bar-item w3-button w3-hide-small" href="#">Control Mensual </a>
+        <a class="w3_bar-item w3-button w3-hide-small" href="#">Inventario</a>
+        <a class="w3_bar-item w3-button w3-hide-small" href="#">Control RBD Frituras</a>   
+        <a class="w3_bar-item w3-button w3-hide-small" href="#">Ventas</a>
+        <a class="w3_bar-item w3-button w3-hide-small" href="#">Freidora</a>
+        <a class="w3-bar-item w3-button w3-hide-small" href="#">Suministros</a>
+        <a class="w3_bar-item w3-button w3-hide-small" href="/..Funciones/logout.php">Salir</a>
+        <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" href="javascript:void(0)"
+            onclick="myFunction()">&#9776;</a>
+        <div id="demo" class="w3-bar-block w3-orange w3-hide w3-hide-large w3-hide-medium">
+                <a class="w3-bar-item w3-button" href="#">Control Mensual </a>
+                <a class="w3-bar-item w3-button" href="#">Inventario</a>
+                <a class="w3-bar-item w3-button" href="#">Control RBD Frituras</a>
+                <a class="w3-bar-item w3-button" href="#">Ventas</a>
+                <a class="w3-bar-item w3-button" href="#">Freidora</a>
+                <a class="w3-bar-item w3-button" href="#">Suministros</a>
+            <a class="w3_bar-item w3-button w3-hide-small" href="../Funciones/logout.php">Salir</a>
+        </div>
+    </div>
+    <?php
+        // Copiar siempre
+    ?>
+    <div class="w3-card w3-margin w3-center">
+        <b>
+            <h2>Ventas</h2>
+        </b>
+    </div>
+    <div class="w3-container">
+        <form id action="../Funciones/FmVentas.php" method="POST">
+            <div class="w3-container w3-cell w3-border">
+                <table class=" w3-table">
+                    <tr>
+
+                        <td><b>Factura:</b></td>
+                        <td>
+                            <input class="w3-input w3-border" type="text" name="factura" value="<?php echo $factura; ?>">
+                           
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Fecha de Venta:</b></td>
+                        <td>
+                           <input class="w3-input w3-border" type="text"  name="fecha" value="<?php echo $fecha; ?>">
+                          
+                         
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Codigo Empleado:</b></td>
+                        <td>
+                         <input class="w3-input w3-border" type="text" name="codigo" disabled  value="<?php echo $_SESSION['id']; ?> ">
+
+                        
+                        </td>
+                    </tr>
+                    <tr>
+                    <td><b>Cantidad de Ventas:</b></td>
+                    <td>
+                    
+                    <input class="w3-input w3-border" type="text" name="cantidad" value="<?php echo $cantidad; ?>">
+                        
+                        </td>
+                        <tr>
+                            
+                        </tr>
+                        <td><b>Total Ventas:</b></td>
+                        <td>
+                       
+                             <input class="w3-input w3-border" type="text"  name="total" value="<?php echo $total; ?>"> 
+
+                        <td>
+
+                <div>
+                    
+                </div>
+
+
+                <button class="w3-button w3-round w3-orange w3-section" type="submit" name="Tipo_Accion"
+                        value="I">agregar</button>
+                        <button class="w3-button w3-round w3-orange w3-section" type="submit" name="Tipo_Accion"
+                        value="A">Modificar</button>
+                    <button class="w3-button w3-round w3-orange w3-section" type="submit" name="Tipo_Accion"
+                        value="E">Eliminar</button>
+                    <button class="w3-button w3-round w3-orange w3-section" type="reset" value="reset">Cancelar</button>
+                        </div>
+                        </div>
+                        
+            <div class="w3-container w3-cell ">
+                <table class="w3-table-all">
+                    <?php
+
+                            //Codigo PHP para Tablas
+                                echo 
+                                    "<tr>
+                                        <th>Factura</th>
+                                        <th>Fecha de Venta</th>
+                                        <th>Codigo de Usuario</th>
+                                        <th>Cantidad Vendidas</th>
+                                        <th>Total Ventas</th>
+                                        <th></th>
+                                    <tr/>";
+                                   while($row=$resultado->fetch_assoc()){?>
+                    <tr>
+                        <td><?php echo $row['factura'];?> </td>
+                        <td><?php echo $row['fech_venta']; ?> </td>
+                        <td><?php echo $row['cod_emp']; ?> </td>
+                        <td><?php echo $row['cant_vend']; ?> </td>
+                        <td><?php echo $row['tot_venta']; ?> </td>
+                      <td>
+                      <center><a href="./mVentas.php?id=<?php echo $row['factura'];?>">Seleccionar</a>
+                            </center>
+                        </td>
+                    </tr>
+                    <?php }
+
+                            ?>
+                </table>
+            </div>
+
+        </form>
+    </div>
+</body>
+<script>
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</html>
+<?php }
+?>
