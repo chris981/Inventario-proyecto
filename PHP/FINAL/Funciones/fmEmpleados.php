4e333fdf0c72979ca2ec54a6e0b5c285b69b87d6
ejormej <jlmt@live.com>
2021-09-17 09:49:40 -0600

    validaciones

--- PHP/FINAL/Funciones/fmEmpleados.php
+++ PHP/FINAL/Funciones/fmEmpleados.php
@@ -20,47 +20,56 @@
     $residencia = $_POST["residencia"];
     $cant_ventas = $_POST["cant_ventas"];
     $cant_din_ventas = $_POST["cant_din_ventas"];
-    $tipo=$_POST["tipo"];
+    $tipo = $_POST["tipo"];
 
-    //Iniciamos programaciones
-    require "conexion.php";
-    try {
-        $query = "CALL SP_Empleados($codigo,'$usuario','$clave','$nombre','$apellido','$residencia','$horario','$tipo','$cant_ventas','$cant_din_ventas','$Tipo_Accion')";
-        if ($mysqli->query($query)) {
-            switch ($Tipo_Accion) {
-                case "I":
+    $query1 = "select * from tabla_empleados where usuario='$usuario'";
+    $resultado = $mysqli->query($query);
+    if (mysqli_num_rows($resultado) !== 0) {
+
+        //Iniciamos programaciones
+        require "conexion.php";
+        try {
+            $query = "CALL SP_Empleados($codigo,'$usuario','$clave','$nombre','$apellido','$residencia','$horario','$tipo','$cant_ventas','$cant_din_ventas','$Tipo_Accion')";
+            if ($mysqli->query($query)) {
+                switch ($Tipo_Accion) {
+                    case "I":
     ?>
-                    <h2>Usuario Ingresado con Exito!</h2>
-                    <a href="../Formularios/mEmpleados.php">
-                        <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
-                    </a>
+                        <h2>Usuario Ingresado con Exito!</h2>
+                        <a href="../Formularios/mEmpleados.php">
+                            <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
+                        </a>
+                    <?php
+                        break;
+                    case "A":
+                    ?>
+                        <h2>Usuario Actualizado con Exito!</h2>
+                        <a href="../Formularios/mEmpleados.php">
+                            <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
+                        </a>
+                    <?php
+                        break;
+                    case "E":
+                    ?>
+                        <h2>Usuario Eliminado con Exito!</h2>
+                        <a href="../Formularios/mEmpleados.php">
+                            <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
+                        </a>
                 <?php
-                    break;
-                case "A":
+                        break;
+                }
+            } else {
                 ?>
-                    <h2>Usuario Actualizado con Exito!</h2>
-                    <a href="../Formularios/mEmpleados.php">
-                        <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
-                    </a>
-                <?php
-                    break;
-                case "E":
-                ?>
-                    <h2>Usuario Eliminado con Exito!</h2>
-                    <a href="../Formularios/mEmpleados.php">
-                        <input class="w3-button w3-round w3-orange w3-section" type="button" value="Regresar">
-                    </a>
+                <h2>Error: <?php echo $mysqli->error; ?></h2>
             <?php
-                    break;
             }
-        } else {
+        } catch (Exception $ex) {
             ?>
-            <h2>Error: <?php echo $mysqli->error; ?></h2>
+            <h2>Error: <?php echo $ex; ?></h2>
         <?php
         }
-    } catch (Exception $ex) {
+    } else {
         ?>
-        <h2>Error: <?php echo $ex; ?></h2>
+        <h2>Error: Nombre de usuario ya existe!</h2>
     <?php
     }
     ?>
